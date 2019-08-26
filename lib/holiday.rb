c0209c6ebc50856db8b65d28b868af301a89be0e
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-26 14:41:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   contacts.each do |person, contact_details_hash|
- binding.pry
+ #binding.pry
 end
 
 
