a6ce61b23377a8c4e82cfa16a724a60f36bc2ed5
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-26 16:13:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do|season,holiday|
 puts "#{season.capitalize}:"
 holiday.each do |holiday, item|
-  puts "#holiday"
+  puts "#holiday.split"
 end
 
 def all_holidays_with_bbq(holiday_hash)
